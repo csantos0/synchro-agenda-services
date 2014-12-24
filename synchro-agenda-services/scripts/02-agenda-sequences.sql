157360b9835fec41b57f74a4a1304c63a8fedd24
Ciro Santos <ciro.santos@synchro.com.br>
2014-12-24 10:56:43 -0200

    inclusao dos DDLs para Oracle
    Signed-off-by: Ciro Santos <ciro.santos@synchro.com.br>

---
+++ synchro-agenda-services/scripts/02-agenda-sequences.sql
@@ -0,0 +1,15 @@
+--------------------------------------------------------
+--  DDL for Sequence SYN_SEQ_ADDRESS
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "CVSASSETS"."SYN_SEQ_ADDRESS"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence SYN_SEQ_CONTACT
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "CVSASSETS"."SYN_SEQ_CONTACT"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence SYN_SEQ_USER
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "CVSASSETS"."SYN_SEQ_USER"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE ;
